9f9651783cf895a2347a64978207c66b672f2649
xiaoxucong <2691664868@qq.com>
2020-07-05 17:45:10 +0800

    作业

---
+++ Week03/Two_TreeNode.java
@@ -0,0 +1,19 @@
+package com.algorithm.ok.week_3;
+
+import com.algorithm.ok.week_2.TreeNode;
+
+/***
+ * 236. 二叉树的最近公共祖先
+ */
+public class Two_TreeNode {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) return null; // 1.
+        if(left == null) return right; // 3.
+        if(right == null) return left; // 4.
+        return root; // 2. if(left != null and right != null)
+    }
+}
